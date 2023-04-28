96b94b09d7beb9a91e8515c0f01ad2efcff65d22
JAIME GÓMEZ GARRIDO <Jaime.Gomez9@alu.uclm.es>
2023-04-28 10:08:35 +0200

    Trabajo Inicial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import Interfaz.MainFrame;
+
+/*
+import Interfaz.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame GUI = new MainFrame();
+    }
+}*/
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        MainFrame GUI = new MainFrame();
+    }
+}
